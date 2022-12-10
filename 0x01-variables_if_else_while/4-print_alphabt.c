148899e33a6911e315a899efe98680f15b02e209
Elorm Dokosi <marriondokosi@gmail.com>
2022-12-09 18:20:06 -0800

    minor text edit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,21 +10,15 @@
 int main(void)
 {
 	char a;
-
 	for (a = 'a'; a <= 'z'; a++)
 	{
 		if (a == 'q' || a == 'e')
-			
 			continue;
-		
 		else 
 		{
 			putchar(a);
 		}
 	}
-	
 	putchar('\n');
-
-
 	return (0);
 }
