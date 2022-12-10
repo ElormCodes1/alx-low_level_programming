2e5989c8074ad81cf160c4384b38ba4a3625d068
Elorm Dokosi <marriondokosi@gmail.com>
2022-12-09 17:23:52 -0800

    all the alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,12 +9,11 @@
 
 int main(void)
 {
-        char alpha [26]= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-        for (int i=0; i<=25; i++)
-        {
-        putchar(alpha[0]);
-        alpha[0]++;
-        }
+        char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
 
 
         return (0);
