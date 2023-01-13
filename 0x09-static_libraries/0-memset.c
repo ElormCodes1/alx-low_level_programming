27ffa8e0e3d6ccb46ab88dc6b674ffdc25dd4dc7
Elorm Dokosi <marriondokosi@gmail.com>
2023-01-13 09:08:01 -0800

    test

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,5 +1,9 @@
 #include "main.h"
 /**
  * _memset - sets memory to array
+ * @s: array to set
+ * @b: value to set it as
+ * @n: n amount of times
+ * Return: char value of s
  */
 char *_memset(char *s, char b, unsigned int n);
