b9dcf679299731ddccef474de19545a9365dea1d
Elorm Dokosi <marriondokosi@gmail.com>
2023-01-14 13:22:45 -0800

    test

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,13 @@
+#include "main.h"
 #include <stdio.h>
+#define UNUSED(x) (void)(x)
 /**
  * @argc: argument counter
  * @argv: argument strings
  */
 int main(int argc, char *argv[])
 {
+	UNUSED(argc);
 	printf("%s\n", argv[0]);
 	return (0);
 }
