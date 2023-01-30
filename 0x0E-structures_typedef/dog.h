ad6d49cb3e36a1ffc31314483d5c31a07930a3e0
Elorm Dokosi <marriondokosi@gmail.com>
2023-01-29 18:45:21 -0800

    main file, readme file and first task file for structures and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - type with properties of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner of dog
+ */
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
