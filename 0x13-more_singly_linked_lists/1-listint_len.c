2cca2dc2938f448167efdef61c379b07a6fd67be
Elorm Dokosi <marriondokosi@gmail.com>
2023-02-01 05:03:11 -0800

    linked list files

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,16 +2,16 @@
 /**
  * size_t listint_len - counts number of elements in a list
  * @h: pinter to a constant variable of type listint_t
+ * Return: amount of elements
  */
 size_t listint_len(const listint_t *h)
 {
 	int counter = 0;
-	listint_t current = NULL;
 
 	while (h->next != NULL)
 	{
 		counter++;
-		h = h->next->next;
+		h = h->next;
 	}
 	return (counter);
 }
