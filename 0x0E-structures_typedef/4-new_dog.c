432c22fd9359ff140635a99902da306b49eb2a1e
Elorm Dokosi <marriondokosi@gmail.com>
2023-02-08 01:09:36 -0800

    correct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * new_dog - creates a struct dog
+ * @name: name of dag
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog_t
+
